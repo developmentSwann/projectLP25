ceb93a1d7321ebc6e7728db6921e2b213840c726
Your Name <development.swann@gmail.com>
2023-12-08 11:31:26 +0100

    Modification configuration (on c'était embrouillé)

--- configuration.h
+++ configuration.h
@@ -10,32 +10,11 @@ typedef struct {
     uint8_t processes_count;
     bool is_parallel;
     bool uses_md5;
+    bool is_dry_run;
+    bool is_verbose;
 } configuration_t;
 
-typedef enum {
-    LPARAM_SOURCE,
-    LPARAM_DESTINATION,
-    LPARAM_DATE_SIZE_ONLY,
-    LPARAM_NO_PARALLEL,
-    LPARAM_DRY_RUN,
-    PARAM_CPU_MULT,
-    PARAM_VERBOSE,
 
-} opt_values;
-
-
-
-typedef union{
-    char str_param[STR_MAX];
-    long long int_param;
-    bool flag_param;
-} type_arg;
-
-
-typedef struct {
-    opt_values parameter_type;
-    type_arg parameter_value;
-} parameter_t;
 
 void init_configuration(configuration_t *the_config);
 int set_configuration(configuration_t *the_config, int argc, char *argv[]);
