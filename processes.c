402766e640fab7fe406b799fb2e9084fd6dbb37a
julesprrt <jules.pierret4582@gmail.com>
2023-12-15 21:20:49 +0100

    fct prepare

--- processes.c
+++ processes.c
@@ -16,7 +16,47 @@
  * @return 0 if all went good, -1 else
  */
 int prepare(configuration_t *the_config, process_context_t *p_context) {
+    if (the_config->is_parallel) {
+        p_context->processes_count = the_config->processes_count;
+        p_context->main_process_pid = getpid();
+        p_context->source_lister_pid = -1;
+        p_context->destination_lister_pid = -1;
+        p_context->source_analyzers_pids = malloc(sizeof(pid_t) * the_config->processes_count);
+        p_context->destination_analyzers_pids = malloc(sizeof(pid_t) * the_config->processes_count);
+        p_context->shared_key = ftok(the_config->source, 1);
+        p_context->message_queue_id = msgget(p_context->shared_key, IPC_CREAT | 0666);
+        if (p_context->message_queue_id < 0) {
+            perror("msgget");
+            return -1;
+        }
+        lister_configuration_t *source_lister_config = malloc(sizeof(lister_configuration_t));
+        source_lister_config->my_recipient_id = SOURCE_LISTER_ID;
+        source_lister_config->my_receiver_id = SOURCE_LISTER_ID;
+        source_lister_config->analyzers_count = the_config->processes_count;
+        source_lister_config->mq_key = p_context->shared_key;
+        p_context->source_lister_pid = make_process(p_context, lister_process_loop, source_lister_config);
+        if (p_context->source_lister_pid < 0) {
+            perror("make_process");
+            return -1;
+        }
+        lister_configuration_t *destination_lister_config = malloc(sizeof(lister_configuration_t));
+        destination_lister_config->my_recipient_id = DESTINATION_LISTER_ID;
+        destination_lister_config->my_receiver_id = DESTINATION_LISTER_ID;
+        destination_lister_config->analyzers_count = the_config->processes_count;
+        destination_lister_config->mq_key = p_context->shared_key;
+        p_context->destination_lister_pid = make_process(p_context, lister_process_loop, destination_lister_config);
+        if (p_context->destination_lister_pid < 0) {
+            perror("make_process");
+            return -1;
+        }
+        for (int i = 0; i < the_config->processes_count; i++) {
+            analyzer_configuration_t *source_analyzer_config = malloc(sizeof(analyzer_configuration_t));
+            source_analyzer_config->my_recipient_id = SOURCE_ANALYZER_ID;
+            source
 
+        }
+    }
+    return 0;
 }
 
 /*!
@@ -73,5 +113,6 @@ void analyzer_process_loop(void *parameters) {
  * @param p_context is a pointer to the processes context
  */
 void clean_processes(configuration_t *the_config, process_context_t *p_context) {
+
 
 }
